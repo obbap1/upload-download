4ac80dc34ea297b64bed1defe0554f3cc1af56b2
obbap1 <paschal.obba@stu.cu.edu.ng>
2019-03-31 03:26:29 +0100

    Fix: fix upload route

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 const http = require('http');
-const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const Busboy = require('busboy');
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(url.parse(req.url));
+  const { url, method } = req;
 
-  if (req.url === '/upload' && req.method === 'GET') {
-    console.log(req);
-    res.end('sure G');
+  // Upload songs
+  if (url === '/upload' && method === 'POST') {
+    const busboy = new Busboy({ headers: req.headers });
+
+    busboy.on('file', (__, file, filename, ___, mimetype) => {
+      fs.open(`${path.join(__dirname, './songs', filename)}`, 'r', (err, _) => {
+        if (err && err.code === 'ENOENT') {
+          const saveTo = path.join(__dirname, './songs', filename);
+          file.pipe(fs.createWriteStream(saveTo));
+        } else {
+          console.log('already here');
+          res.end('File already exists!');
+        }
+      });
+
+      if (mimetype !== 'audio/mp3') {
+        res.statusCode = 403;
+        res.end('Only mp3 files are accepted');
+      }
+    });
+
+    busboy.on('finish', () => {
+      console.log('Done parsing form!!!');
+      res.end('Your File has been uploaded successfully');
+      res.writeHead(303, { Connection: 'close', Location: '/' });
+    });
+
+    req.pipe(busboy);
   }
 
-  res.end('hello');
+  // See songs
+
+
+  // Download songs
 });
 
 server.listen(port, () => console.log(`This server runs on ${port}`));
